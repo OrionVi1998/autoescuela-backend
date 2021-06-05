8426a99e59bd7b90dc1717bd84bf34bfb8f6386b
OrionVi <octaviov1998@gmail.com>
2021-06-05 18:26:26 -0300

    Changes by francisco

--- Modelo/storebroker.js
+++ Modelo/storebroker.js
@@ -20,6 +20,82 @@ class Storebroker {
         }
     }
 
+
+    static async crearProfesor(profesor) {
+        let conn;
+        try {
+            conn = await pool.getConnection(); // 1 = administrador & 0 = profesores
+            // columnas: (email, credencial, nombre, apellido, telefono, direccion, horaInicio, horaFin)
+            const rows = await conn.query(
+                `INSERT INTO usuarios (email, credencial, nombre, apellido, telefono, direccion, horaInicio, horaFin) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+                [profesor.email, 0, profesor.nombre, profesor.apellido, profesor.telefono, profesor.direccion, profesor.horaInicio, profesor.horaFin]);
+            console.log(rows); // FIXME
+
+        } catch (err) {
+            throw err;
+        } finally {
+            if (conn) return conn.end;
+        }
+    }
+
+
+    static async editarProfesor(profesor) {
+
+        let conn;
+        try {
+            conn = await pool.getConnection(); // 1 = administrador & 0 = profesores
+            // columnas: (email, credencial, nombre, apellido, telefono, direccion, horaInicio, horaFin)
+            const rows = await conn.query("UPDATE usuarios SET email=?, credencial=?, nombre=?, apellido=?, telefono=?, direccion=?, horaInicio=?, horaFin=? WHERE ID_USUARIO=?",
+                [profesor.email, 0, profesor.nombre, profesor.apellido, profesor.telefono, profesor.direccion, profesor.horaInicio, profesor.horaFin, profesor.id_usuario]);
+            console.log(rows); // FIXME
+
+
+        } catch (err) {
+            throw err;
+        } finally {
+            if (conn) return conn.end;
+        }
+
+    }
+
+    static async eliminarProfesor() {
+    }
+
+    static async crearAlumnos() {
+    }
+
+    static async editarAlumnos() {
+    }
+
+    static async eliminarAlumnos() {
+    }
+
+    static async crearPaquete() {
+    }
+
+    static async editarPaquete() {
+    }
+
+    static async eliminarPaquete() {
+    }
+
+    static async crearPago() {
+    }
+
+    static async editarPago() {
+    }
+
+    static async eliminarPago() {
+    }
+
+    static async crearTurnos() {
+    }
+
+    static async editarTurnos() {
+    }
+
+    static async eliminarTurnos() {
+    }
 }
 
 // Storebroker.getProfesores()
@@ -28,7 +104,7 @@ let p = new Profesor(
     3,
     "presidenta@somalia.haha",
     0,
-    "Somalia",
+    "Langosta",
     "Luna",
     666666666,
     "Somalia 321",
