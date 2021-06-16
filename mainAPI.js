2151b4528efa0b465034b35d105941830a0c2068
OrionVi <octaviov1998@gmail.com>
2021-06-16 09:07:03 -0300

    Minor

--- mainAPI.js
+++ mainAPI.js
@@ -40,51 +40,51 @@ init().then(() => {
 
 
 api.use(cors({
-    "origin": process.env.CORS_ORIGIN, //https://proyecto-autoescuela.web.app/
+    "origin": process.env.CORS_ORIGIN,
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
     "optionsSuccessStatus": 204
 }))
 api.use(express.json())
 
-api.get("/login", (req, res) => {
+api.get(`/login`, (req, res) => {
     let admin = contenedorAdministrador.getAdministradores()
     let profesor = contenedorProfesor.getProfesores()
 
     admin = admin.find(a => a.email === req.query.email)
     profesor = profesor.find(p => p.email === req.query.email)
 
-    console.log(`LOGIN ATTEMPT: ${req.query.email}`)
+    console.log(`LOGIN ATTEMPT: ${req.query.email} - A: ${admin} - P: ${profesor} `)
 
     if (admin) {res.send({session: true, credencial:admin.credencial})}
-    else if (profesor) {res.send({session: false, credencial:profesor.credencial})}
+    else if (profesor) {res.send({session: true, credencial:profesor.credencial})}
     else {res.send({session:false})}
 
 })
 
-api.get("/", (req, res) => {
-    res.send({value: "Hello postman"})
+api.get(`/`, (req, res) => {
+    res.send({value: `Hello postman`})
 })
 
-api.get("/getPaquetes/", (req, res) => {
+api.get(`/getPaquetes/`, (req, res) => {
     let paquetes_retorno = contenedorPaquete.getPaquetesVisibles()
-    console.log("GET PAQUETES - ENVIANDO")
+    console.log(`GET PAQUETES - ENVIANDO`)
     res.send(paquetes_retorno)
 })
 
-api.get("/getPagos/", (req, res) => {
+api.get(`/getPagos/`, (req, res) => {
     let pagos_retorno = contenedorPagos.getPagos()
-    console.log("GET PAGOS - ENVIANDO")
+    console.log(`GET PAGOS - ENVIANDO`)
     res.send(pagos_retorno)
 })
 
-api.get("/getPagosAlumno", ((req, res) => {
+api.get(`/getPagosAlumno`, ((req, res) => {
     let pagos_alumno_ret = mediadorPagosPaqueteAlumnos(contenedorPagos, contenedorPaquete, Number(req.query.id_alumno))
     console.log(`GET PAGOS ALUMNO ${req.query.id_alumno} - ENVIANDO`)
     res.send(pagos_alumno_ret)
 }))
 
-api.put("/registrarPago", ((req, res) => {
+api.put(`/registrarPago`, ((req, res) => {
 
     try {
         let pago = contenedorPagos.getPago(req.body.params)
@@ -96,9 +96,9 @@ api.put("/registrarPago", ((req, res) => {
 
 }))
 
-api.get("/getTurnos/", (req, res) => {
+api.get(`/getTurnos/`, (req, res) => {
     let turnos_retorno = contenedorTurno.getTurnos()
-    console.log("GET TURNOS - ENVIANDO")
+    console.log(`GET TURNOS - ENVIANDO`)
 
     turnos_retorno.map((t) => {
 
@@ -112,25 +112,25 @@ api.get("/getTurnos/", (req, res) => {
     res.send(turnos_retorno)
 })
 
-api.get("/getAlumnos/", (req, res) => {
+api.get(`/getAlumnos/`, (req, res) => {
     let alumnos_retorno = contenedorAlumno.getAlumnos()
-    console.log("GET ALUMNOS - ENVIANDO")
+    console.log(`GET ALUMNOS - ENVIANDO`)
     res.send(alumnos_retorno)
 })
 
-api.get("/getProfesores/", (req, res) => {
+api.get(`/getProfesores/`, (req, res) => {
     let profesores_retorno = contenedorProfesor.getProfesores()
-    console.log("GET PROFESORES - ENVIANDO")
+    console.log(`GET PROFESORES - ENVIANDO`)
     res.send(profesores_retorno)
 })
 
-api.get("/getAdministradores/", (req, res) => {
+api.get(`/getAdministradores/`, (req, res) => {
     let administradores_retorno = contenedorAdministrador.getAdministradores()
-    console.log("GET ADMINISTRADORES")
+    console.log(`GET ADMINISTRADORES`)
     res.send(administradores_retorno)
 })
 
-api.put("/crearAlumno/", (req, res) => {
+api.put(`/crearAlumno/`, (req, res) => {
     //
     console.log(req.body) // objeto json con el alumno
     let nombre, apellido, telefono, direccion;
@@ -146,7 +146,7 @@ api.put("/crearAlumno/", (req, res) => {
 })
 
 
-api.delete("/eliminarProfesor", (req, res) => {
+api.delete(`/eliminarProfesor`, (req, res) => {
     let profesores_eliminar = req.body // {nombres ...}
     // contenedorProfesor.eliminarProfesor(profesores_eliminar)
     // contenedorTurno.desvincularProfesor(profesores_eliminar)
